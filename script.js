6dd26663fbf5a098003960ba6a79aa9cf15822f0
Wolfgang <wolfgangkirstein7@gmail.com>
2022-11-03 17:05:38 -0400

    Finish V2 create ui able to change grid size

--- script.js
+++ script.js
@@ -2,14 +2,27 @@ const container = document.querySelector("#container");
 
 const dimension = 16;
 
-for (let i = 0; i < dimension; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row')
-    container.appendChild(row)
+generateGrid(dimension);
 
-    for (let j = 0; j < dimension; j++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        row.appendChild(square);
-    }
+function generateGrid(dimension) {
+
+       for (let i = 0; i < dimension; i++) {
+       const row = document.createElement('div');
+       row.classList.add('row')
+       container.appendChild(row)
+
+       for (let j = 0; j < dimension; j++) {
+           const square = document.createElement('div');
+           square.classList.add('square');
+           row.appendChild(square);
+       }
+    }   
+}
+
+function changeGrid() {
+    const node = document.getElementById('container');
+    node.innerHTML = '';
+    const newDimension = prompt("Number of Squares Per Side (must be no more than 100)")
+    if (newDimension > 100) changeGrid();
+    generateGrid(newDimension);
 }
